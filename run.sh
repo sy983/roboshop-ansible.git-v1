4321b348c8ca29e44171520c2cf69a8938135c0e
admin <suhasy983@gmail.com>
2025-01-04 19:50:50 +0530

    robo-ansible all pass updated in vault

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1.dev.sydevops80.online, -e env=dev -e ansible-user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
+ansible-playbook -i $1.dev.sydevops80.online, -e env=dev -e ansible-user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml -e vault_token=$2
